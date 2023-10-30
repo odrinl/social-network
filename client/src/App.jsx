5eb68f18587829963db5792c36e29813623c0b3d
OK-Ro <o.robert1994@hotmail.com>
2023-10-30 13:00:47 +0100

    houten

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import CreateUser from "./pages/User/CreateUser";
-import UserList from "./pages/User/UserList";
-import Welcome from "./pages/Home/Welcome";
-import LoginUser from "./pages/User/LoginUser";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home/Home";
+// import CreateUser from "./pages/User/CreateUser";
+// import UserList from "./pages/User/UserList";
+// import Welcome from "./pages/Home/Welcome";
+// import LoginUser from "./pages/User/LoginUser";
 
-const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/users" element={<UserList />} />
-        <Route path="/login" element={<LoginUser />} />
-        <Route path="/register" element={<CreateUser />} />
-      </Routes>
-    </>
-  );
-};
+// const App = () => {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Welcome />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/home/users" element={<UserList />} />
+//         <Route path="/login" element={<LoginUser />} />
+//         <Route path="/register" element={<CreateUser />} />
+//       </Routes>
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
