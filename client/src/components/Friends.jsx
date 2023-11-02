8e831999b327ecfaab307887f13cfc0e126cdab0
molhamalrashed <molham.alrd@gmail.com>
2023-11-02 15:26:12 +0100

    pull

--- client/src/components/Friends.jsx
+++ client/src/components/Friends.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import styled from "styled-components";
 
 const Friends = () => {
+const [endpoint, setEndpoint] = useState("")
+
+const endPointHandler = ()=>{
+}
   return (
     <Container>
       <div className="container block">
         <h2>Friends</h2>
       </div>
+      <SearchDiv>
+        <search>
+          </search>
+      </SearchDiv>
+      <FriendsNav>
+        <ul>
+          <li onClick={()=>{
+            setEndpoint("")
+            endPointHandler()
+          }
+        }>Friends{endpoint}</li>
+          <li onClick={setEndpoint("/friends/friend-request:id${}")}>Friends Requests</li>
+          <li>Sent Requests</li>
+        </ul>
+      </FriendsNav>
     </Container>
   );
 };
@@ -15,3 +34,10 @@ export default Friends;
 const Container = styled.div`
   height: 20rem;
 `;
+
+
+const FriendsNav = styled.div`
+`
+
+const SearchDiv = styled.div`
+`
