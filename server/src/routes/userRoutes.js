608dcf13e7111342e8f41e5f77661564ae0396b2
Ahmet Avci <ahmetavci.8873@gmail.com>
2023-11-05 11:16:03 +0100

    cancel-request

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -9,6 +9,7 @@ import {
   getAllSentRequests,
   getAllReceivedRequests,
   searchNonFriendsByName,
+  cancelFriendRequest,
 } from "../controllers/userControllers.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
@@ -18,6 +19,8 @@ router.get("/:id", protect, getUser);
 router.get("/:id/friends", protect, getUserFriends);
 
 router.post("/:senderId/:receiverId", protect, sendFriendRequest);
+router.delete("/:senderId/:receiverId/cancel", protect, cancelFriendRequest);
+
 router.put("/:senderId/:receiverId/accept", protect, acceptFriendRequest);
 router.put("/:senderId/:receiverId/reject", protect, rejectFriendRequest);
 
