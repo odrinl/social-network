8fd706d1c648af34a200868aa93f8bed523795f0
molhamalrashed <molham.alrd@gmail.com>
2023-11-11 18:16:48 +0100

    welcome page

--- client/src/pages/Home/Welcome.jsx
+++ client/src/pages/Home/Welcome.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Welcome = () => {
   return (
-    <div>
+    <Container>
       <h1>This is the Welcome Page</h1>
       <p>Good luck with the project, Group A!</p>
 
       <ul>
-        <Link to="/home">
-          <li>Home</li>
-        </Link>
         <Link to="/login">
           <li>Login</li>
         </Link>
@@ -18,8 +16,29 @@ const Welcome = () => {
           <li>Register</li>
         </Link>
       </ul>
-    </div>
+    </Container>
   );
 };
 
 export default Welcome;
+
+
+const Container = styled.div`
+  min-width: 40%;
+  height: 85vh;
+  margin: 30px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(255, 255, 255);
+  padding: 40px;
+  box-shadow: 0px 0px 50px rgba(0, 0, 0, 0.3);
+  position: relative;
+  overflow: hidden;
+  border-radius: 20px;
+  border: 3px solid #90467f;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
