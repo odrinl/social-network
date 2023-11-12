6ee34e70b352549deb7aba83c9fc484ba9279337
OK-Ro <o.robert1994@hotmail.com>
2023-11-12 12:56:47 +0100

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,28 +8,25 @@ import LoginUser from "./pages/User/LoginUser";
 import ProfilePage from "./pages/Profile/ProfilePage";
 import SharedArea from "./components/SharedArea";
 import Feed from "./components/Feed";
-
 import Friends from "./components/Friends";
 import Myprofile from "./components/profile/Myprofile";
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/home" element={<Home />}>
-          <Route index element={<SharedArea />} />
-          <Route path="/home/" element={<Feed />} />
-          <Route path="/home/myprofile/" element={<Myprofile />} />
-          <Route path="/home/friends" element={<Friends />} />
-        </Route>
-        <Route path="/home/users" element={<UserList />} />
-        <Route path="/login" element={<LoginUser />} />
-        <Route path="/register" element={<CreateUser />} />
-        <Route path="/home/my-profile/" element={<ProfilePage />} />
-        <Route path="/home/my-profile/:userId" element={<ProfilePage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/home" element={<Home />}>
+        <Route index element={<SharedArea />} />
+        <Route path="*" element={<Feed />} />
+        <Route path="myprofile/*" element={<Myprofile />} />
+        <Route path="friends/*" element={<Friends />} />
+      </Route>
+      <Route path="/home/users" element={<UserList />} />
+      <Route path="/login" element={<LoginUser />} />
+      <Route path="/register" element={<CreateUser />} />
+      <Route path="my-profile/*" element={<ProfilePage />} />
+      <Route path="my-profile/:userId" element={<ProfilePage />} />
+    </Routes>
   );
 };
 
