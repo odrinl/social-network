c7b5b3fb301fcad1a0b17c82cb61095c80720902
Ahmet Avci <ahmetavci.8873@gmail.com>
2023-11-14 12:18:38 +0100

    upload-image

--- server/src/routes/uploadRoutes.js
+++ server/src/routes/uploadRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-
+import User from "../models/User.js";
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -14,8 +14,28 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.post("/post-image", upload.single("image"), (req, res) => {
-  res.json({ success: true, imageUrl: req.file.filename });
-});
+router.post(
+  "/upload-profile-picture/:userId",
+  upload.single("profilePicture"),
+  async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const user = await User.findById(userId);
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ success: false, error: "User not found" });
+      }
+
+      user.profilePicture = req.file.filename;
+      await user.save();
+
+      res.json({ success: true, profilePictureUrl: user.profilePicture });
+    } catch (error) {
+      res.status(500).send({ success: false, error: "Internal Server Error" });
+    }
+  }
+);
 
 export default router;
