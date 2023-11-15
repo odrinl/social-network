0ce030d765ece29c799e2952935efe4b58439600
Ahmet Avci <ahmetavci.8873@gmail.com>
2023-11-15 12:18:15 +0100

    upload

--- server/src/routes/uploadRoutes.js
+++ server/src/routes/uploadRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads"); // Save files directly in the "uploads" directory
+    cb(null, "uploads");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
